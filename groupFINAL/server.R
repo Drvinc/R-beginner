31a454256fdfae62bcf8af6d4c144274df71009a
Drvinc <t24735281@gmail.com>
2016-07-22 14:45:32 +0800

    Shinyapps

--- groupFINAL/server.R
+++ groupFINAL/server.R
@@ -1,51 +1,148 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(ggmap)
+library(mapproj)
+library(ggplot2)
 
-AllData <- read.csv("SinyiClean.csv")
+AllData <- read.csv("SinyiDist.csv")
 target <- AllData[AllData$TotPr<12000,]
 target <- target[target$MetPr<250,]
-target <- target[target$ConstMet<160,]
-target <- target[target$LandMet<50,]
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  
-  output$TotPrPlot <- renderPlot({
-    x <- target$TotPr
-    bins <- seq(min(x), max(x), length.out = input$bins)
-    # generate bins based on input$bins from ui.R
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white', main="台北市房價分布(總價)" , xlab="總價(萬)", ylab="件數")
+AreaList <- c("Song-Shan","Sin-Yi","Da-An","Chung-Shan","Chung-Cheng","Da-Tung","Wan-Hua","Wen-Shan","Nan-Gang","Nei-Hu","Shi-Lin","Bei-Tou")
+
+shinyServer(function(input, output, session) {
+  observe({
+    if(input$select_none > 0){
+      if(input$select_none %% 2 == 0){
+        updateCheckboxGroupInput(session,"Area",selected=c(1:12))
+      }
+      else{
+        updateCheckboxGroupInput(session,"Area",selected=c())
+      } 
+    }
+  })
+  #TAB1
+  output$MetPrArea <- renderPlot({
+    A_select <- as.vector(input$Area)
+    tarr <- subset(target, Area %in% A_select)
+    df = subset(tarr, select=c("Area","MetPr"))
+    bymedian <- with(df, reorder(Area, MetPr, median)) #sort by median
+    A_select <- as.vector(levels(bymedian))
+    A_names <- vector(mode="character",length=length(A_select))
+    for (i in 1:length(A_select))
+    {A_names[i] <- as.character(AreaList[as.numeric(A_select[i])])}
+    box <- ggplot(data = df,aes(x=bymedian , y=MetPr,fill = bymedian)) + coord_cartesian(ylim = c(10, 200))
+    box <- box + geom_boxplot(outlier.size = 1,outlier.colour = "gray",position = "dodge",colour = "#666666",width = 0.5)
+    box <- box + labs(x = "Area", y = "Per Ping price (10K)", title = "Price Comparison by Area")
+    box
+  })
+  output$TPEitems <- renderPlot({
+    A_select <- as.vector(input$Area)
+    tarr <- subset(target, Area %in% A_select)
+    # plot items on TPE city
+    x <- tarr$MetPr
+    tarr$diff <- with(tarr, (MetPr-min(x))/(max(x)-min(x)))
+    p <- ggmap(get_map(location=c(lon=121.545,lat=25.0545), zoom=12, maptype = "roadmap"))
+    p <- p + geom_point(data=tarr,aes(Response_X,Response_Y,colour=diff,size=diff))
+    p <- p + scale_color_gradient2(low="black",mid="blue",high = "red",midpoint = 0.4)
+    p <- p + scale_size(range=c(0.1,2.5))
+    p
+  })
+  output$AvgCompare <- renderPlot({
+    A_select <- as.vector(input$Area)
+    meanPrice <- data.frame("Area"=c(1:13),"MeanMetPr"=0)
+    for (i in 1:12){
+      tarr <- target[(target$Area == i),]
+      meanPrice$MeanMetPr[i] <- mean(tarr$MetPr,na.rm=TRUE)
+    }
+    meanPrice$MeanMetPr[13]=mean(target$MetPr,na.rm=TRUE)
+    
+    x = A_select
+    df <- meanPrice[meanPrice$Area %in% c(x,13),]
+    A_names <- vector(mode="character",length=length(A_select))
+    for (i in 1:length(A_select))
+    {A_names[i] <- as.character(AreaList[as.numeric(A_select[i])])}
+    A_names <- c(A_names, "TPE") 
+    #barplot(height=df$MeanMetPr, horiz=TRUE, names.arg = A_names)
+    df$Area = factor(df$Area,levels = c(1,2,3,4,5,6,7,8,9,10,11,12,13))
+    c <- ggplot(df, aes(x = df$Area , y = df$MeanMetPr,fill = Area))
+    c <- c + geom_bar(stat="identity",width = 0.5)
+    c <- c +labs(x = "Area", y = "Avg per Ping price (10K)")
+    c 
   })
   
-  output$MetPrPlot <- renderPlot({
-    x <- target$MetPr
-    bins <- seq(min(x), max(x), length.out = input$bins)
-    hist(x, breaks = bins, col = 'darkgray', border = 'white', main="台北市房價分布(每坪)" , xlab="每坪價格(萬)", ylab="件數")
+  #TAB2
+  output$MetPrHist <- renderPlot({
+    A_select <- input$Area1
+    tarr <- target
+    tarr1 <- target[target$Area==A_select,]
+    if(A_select != 0){tarr <- tarr1}
+    df = subset(tarr, select=c("Area","MetPr","YyySe"))
+    
+    Y1 <- as.numeric(input$Year[1])
+    Y2 <- as.numeric(input$Year[2])
+    SeN <- ((Y2-Y1)/0.25)+1
+    YSe <- vector(mode="character",length = SeN+1)
+    int1 <- as.integer(Y1)
+    dec1 <- Y1-as.integer(Y1)
+    for (i in 1:SeN){
+      YSe[i] <- paste(int1+as.integer(dec1+(i-1)*0.25),(dec1+(i-1)*0.25-as.integer(dec1+(i-1)*0.25))/0.25+1,sep="")
+    }
+    
+    df = subset(tarr, YyySe %in% YSe)
+    bins <- input$bins
+    hist(df$MetPr, breaks=bins, col="gray",xlab = "Per Ping price (10K)", ylab = "Deal number (counts)", main=paste(ifelse(A_select==0,"Taipei ",AreaList[as.numeric(A_select)]),"'s Distribution of per Ping Price: ",YSe[1]," to ",YSe[SeN],sep=""))
+  })
+  output$MetPrTrend <- renderPlot({
+    A_select <- input$Area1
+    tarr <- target
+    tarr2 <- target[target$Area==A_select,]
+    
+    Y1 <- as.numeric(input$Year[1])
+    Y2 <- as.numeric(input$Year[2])
+    SeN <- ((Y2-Y1)/0.25)+1
+    YSe <- vector(mode="character",length = SeN+1)
+    int1 <- as.integer(Y1)
+    dec1 <- Y1-as.integer(Y1)
+    for (i in 1:SeN){
+      YSe[i] <- paste(int1+as.integer(dec1+(i-1)*0.25),(dec1+(i-1)*0.25-as.integer(dec1+(i-1)*0.25))/0.25+1,sep="")
+    }
+    if (A_select==0){tarr2 <- target}
+    tarr2 = subset(tarr2, YyySe %in% YSe)
+    
+    YSeRange <- levels(as.factor(tarr2$YyySe))
+    meanPr2 <- data.frame("YyySe"=YSeRange,"MeanMetPr"=0)
+    for (i in 1:length(YSeRange)){
+      tarrM <- tarr2[(tarr$YyySe == YSeRange[i]),]
+      meanPr2$MeanMetPr[i] <- mean(tarrM$MetPr,na.rm=TRUE)
+      meanPr2$Year[i] <- substr(meanPr2$YyySe[i], start=1, stop=3)
+      meanPr2$Season[i] <- substr(meanPr2$YyySe[i], start=4, stop=4)
+    }
+    
+    p <- ggplot(meanPr2, aes(x = YyySe, y = MeanMetPr , color = Season, group=1)) + geom_point(size = 3.8) + geom_line(size = 0.8) 
+    p <- p +labs(x = "Season", y = "Avg per Ping price (10K)", title=paste(ifelse(A_select==0,"Taipei",AreaList[as.numeric(A_select)]),"'s Trend: ",YSe[1]," to ",YSe[SeN],sep=""))
+    p
+  })
+  output$DistMRT <- renderPlot({
+    A_select <- input$Area1
+    tarr <- target
+    tarr3 <- target[target$Area==A_select,]
+    if (A_select==0){tarr3 <- target}
+    
+    NearCrit <- input$Distnce
+    tarr3$DistNear <- ifelse(tarr3$distMRT<NearCrit,TRUE,FALSE)
+    TT <- t.test(MetPr~DistNear,tarr3, paired=FALSE)
+    box <- ggplot(data=tarr3 ,aes(x=DistNear , y=MetPr,fill = DistNear)) 
+    box <- box + geom_boxplot(outlier.size = 2, outlier.shape = 1,outlier.colour = "gray",position = "dodge",colour = "#666666",width = 0.5)
+    box <- box + labs(x = "Near MRT or Not", y = "Per Ping price (10K)", title = paste0(ifelse(A_select==0,"Taipei",AreaList[as.numeric(A_select)])," Near MRT vs. Far: 1033 to 1053"))
+    box
   })
   
-  output$ConsMetPlot <- renderPlot({
-    x <- target$ConstMet
-    bins <- seq(min(x), max(x), length.out = input$bins)
-    hist(x, breaks = bins, col = 'darkgray', border = 'white', main="建坪分布" , xlab="坪數(坪)", ylab="件數")
-  })
-  
-  output$LandMetPlot <- renderPlot({
-    x <- target$LandMet
-    bins <- seq(min(x), max(x), length.out = input$bins)
-    hist(x, breaks = bins, col = 'darkgray', border = 'white', main="地坪分布" , xlab="坪數(坪)", ylab="件數")
-  })
-  
-  output$MetTotPlot <- renderPlot({
-  target = AllData[AllData$TotPr<12000,]
-  target = target[target$MetPr<250,]
-  plot(target$MetPr, target$TotPr, pch=20, cex=0.3, main="總價對每坪價格", xlab="每坪價格(萬)", ylab="總價(萬)")
-  })
-  
+  #TAB3
+  output$preImage <- renderImage({
+    filename <- normalizePath(file.path('./images',
+                                        paste('image', input$StatType, '.png', sep='')))
+    list(src = filename,
+         alt = paste("Image number", input$n))
+    
+  }, deleteFile = FALSE)
 })
--- groupFINAL/ui.R
+++ groupFINAL/ui.R
@@ -1,34 +1,67 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("台北市房價分析"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "區間數:",
-                  min = 5,
-                  max = 100,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("TotPrPlot"),
-      plotOutput("MetPrPlot"),
-      plotOutput("ConsMetPlot"),
-      plotOutput("LandMetPlot"),
-      plotOutput("MetTotPlot")
-    )
-  )
+shinyUI(navbarPage("台北市房價分析",
+                   tabPanel("房價地圖",
+                            sidebarLayout(
+                              sidebarPanel(
+                                actionLink("select_none","清空"),
+                                checkboxGroupInput("Area", 
+                                                   label = h2("行政區"), 
+                                                   choices = list("1.松山區"=1,"2.信義區"=2,"3.大安區"=3,"4.中山區"=4,"5.中正區"=5,"6.大同區"=6,"7.萬華區"=7,"8.文山區"=8,"9.南港區"=9,"10.內湖區"=10,"11.士林區"=11,"12.北投區"=12),
+                                                   selected = c(3,7))
+                              ),
+                              mainPanel(
+                                plotOutput("TPEitems", height="900px"),
+                                plotOutput("MetPrArea", height="400px"),
+                                plotOutput("AvgCompare", height="400px")
+                              )
+                            )
+                   ),
+                   tabPanel("房價統計",
+                            sidebarLayout(
+                              sidebarPanel(
+                                selectInput("Area1", label = "行政區", 
+                                            choices = list("台北市"=0,"松山區"=1,"信義區"=2,"大安區"=3,"中山區"=4,"中正區"=5,"大同區"=6,"萬華區"=7,"文山區"=8,"南港區"=9,"內湖區"=10,"士林區"=11,"北投區"=12),
+                                            selected = 1),
+                                sliderInput("Year", "時間範圍(季)",
+                                            103.5, 105.5, c(103.5,105.5), step = 0.25),
+                                sliderInput("bins",
+                                            "價格分布圖區間數:",
+                                            min = 5,
+                                            max = 100,
+                                            value = 30,step=5),
+                                sliderInput("Distnce",
+                                            "近捷運站標準--小於___公尺:",
+                                            min = 400,
+                                            max = 2000,
+                                            value = 1200, step=100)
+                              ),
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                plotOutput("MetPrHist"),
+                                plotOutput("MetPrTrend"),
+                                plotOutput("DistMRT")
+                              )
+                            )
+                   ),
+                   tabPanel("其他統計",
+                            sidebarLayout(
+                              sidebarPanel(
+                                selectInput("StatType", label = "統計項目", 
+                                            choices = list("地區平均房價對收支--1"=1,"地區平均房價對收支--2"=2,"地區平均房價對收支--回歸1"=3,"地區平均房價對收支--回歸2"=4,"地區平均房價對收支--回歸3"=5,"格局圓餅圖"=6),
+                                            selected = 1)
+                              ),
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                plotOutput("preImage")
+                              )
+                            )
+                   ),
+                   tabPanel("關於"
+                            
+                            
+                   ) 
+                   
 ))
