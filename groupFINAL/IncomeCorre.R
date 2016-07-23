1772ab02fb523651dc68c39ceff6926f194b9303
Drvinc <t24735281@gmail.com>
2016-07-23 23:59:38 +0800

    房價對所得

--- groupFINAL/IncomeCorre.R
+++ groupFINAL/IncomeCorre.R
@@ -6,6 +6,7 @@ AreaIndex <- data.frame(Index=1:12, Area=AreaList)
 
 # calculate mean MetPr
 All12 = allData[allData$TotPr<12000,] # cut extreme values
+All12 = All12[All12$MetPr<250,] # cut extreme values
 meanPrice <- data.frame("Area"=c(1:12),"MeanMetPr"=0)
 for (i in 1:nrow(meanPrice)){
   target <- All12[(All12$Area == i),]
@@ -79,3 +80,53 @@ DF <- data.frame(VAR1=FINAL$EndIncome, VAR2=FINAL$MeanMetPr)
 with(DF, plot(VAR1, VAR2, xlab=x_lab, ylab=y_lab))
 abline(fit <- lm(VAR2 ~ VAR1, data=DF), col='red')
 legend("topleft", bty="n", legend=paste("r square = ", format(summary(fit)$adj.r.squared, digits=4)))
+
+# correlation by ggplot2
+library(ggplot2)
+library(ggrepel)
+library(grid)
+
+DF <- data.frame(VAR1=FINAL$Saving/10000, VAR2=FINAL$MeanMetPr, Area=FINAL$Area)
+rownames(DF) <- AreaList[DF$Area]
+reg <- lm(VAR2~VAR1,data=DF)
+coeff <- coefficients(reg)
+eq = paste0("y = ", round(coeff[2],2), "*x ", ifelse(coeff[1]<0, "- ", "+ "), abs(round(coeff[1],2)))
+r_sq = paste0("R^2 = ", round(summary(reg)$adj.r.squared, 2))
+p <- ggplot(DF,aes(VAR1, VAR2, label = rownames(DF)))
+p <- p + geom_point() + geom_smooth(method=lm, fill='lightblue', color='darkblue', size=1)
+p <- p + geom_label_repel(size=3,aes(label=rownames(DF)))
+p <- p + labs(x = "儲蓄(萬)", y = "平均每坪價格(萬)", title = "房價對儲蓄") 
+annot <- grobTree(textGrob(eq, x=0.05, y=0.9, hjust=0, gp=gpar(col="black", fontsize=13, fontface="bold")),
+                  textGrob(r_sq, x=0.05, y=0.85, hjust=0 , gp=gpar(col="black", fontsize=13, fontface="bold")))
+p <- p+ annotation_custom(annot)
+p # MrtPr to Saving
+
+DF <- data.frame(VAR1=FINAL$DominIncome/10000, VAR2=FINAL$MeanMetPr, Area=FINAL$Area)
+rownames(DF) <- AreaList[DF$Area]
+reg <- lm(VAR2~VAR1,data=DF)
+coeff <- coefficients(reg)
+eq = paste0("y = ", round(coeff[2],2), "*x ", ifelse(coeff[1]<0, "- ", "+ "), abs(round(coeff[1],2)))
+r_sq = paste0("R^2 = ", round(summary(reg)$adj.r.squared, 2))
+p <- ggplot(DF,aes(VAR1, VAR2, label = rownames(DF)))
+p <- p + geom_point() + geom_smooth(method=lm, fill='lightblue', color='darkblue', size=1)
+p <- p + geom_label_repel(size=3,aes(label=rownames(DF)))
+p <- p + labs(x = "可支配所得(萬)", y = "平均每坪價格(萬)", title = "房價對可支配所得") 
+annot <- grobTree(textGrob(eq, x=0.05, y=0.9, hjust=0, gp=gpar(col="black", fontsize=13, fontface="bold")),
+                  textGrob(r_sq, x=0.05, y=0.85, hjust=0 , gp=gpar(col="black", fontsize=13, fontface="bold")))
+p <- p+ annotation_custom(annot)
+p # MrtPr to DominIncome
+
+DF <- data.frame(VAR1=FINAL$EndIncome/10000, VAR2=FINAL$MeanMetPr, Area=FINAL$Area)
+rownames(DF) <- AreaList[DF$Area]
+reg <- lm(VAR2~VAR1,data=DF)
+coeff <- coefficients(reg)
+eq = paste0("y = ", round(coeff[2],2), "*x ", ifelse(coeff[1]<0, "- ", "+ "), abs(round(coeff[1],2)))
+r_sq = paste0("R^2 = ", round(summary(reg)$adj.r.squared, 2))
+p <- ggplot(DF,aes(VAR1, VAR2, label = rownames(DF)))
+p <- p + geom_point() + geom_smooth(method=lm, fill='lightblue', color='darkblue', size=1)
+p <- p + geom_label_repel(size=3,aes(label=rownames(DF)))
+p <- p + labs(x = "所得總額(萬)", y = "平均每坪價格(萬)", title = "房價對所得總額") 
+annot <- grobTree(textGrob(eq, x=0.05, y=0.9, hjust=0, gp=gpar(col="black", fontsize=13, fontface="bold")),
+                  textGrob(r_sq, x=0.05, y=0.85, hjust=0 , gp=gpar(col="black", fontsize=13, fontface="bold")))
+p <- p+ annotation_custom(annot)
+p # MrtPr to EndIncome
